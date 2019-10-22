7515cc6c4136e02891cf9771342029ecc832bf98
Bretton B Holmes <famunkwriter@me.com>
2019-10-22 00:34:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(source_array)
+  array = []
+  counter = 0
+  while counter < source_array.length do
+    array.push(yield(source_array[counter]))
+    counter += 1
+  end
+array
+end
+
+def reduce(source_array)
+  counter = 0
+  array=[]
+  while counter < source_array.size do
+    total += array.push(yield(source_array[counter]))
+    counter += 1
+  end
+  array
+end
