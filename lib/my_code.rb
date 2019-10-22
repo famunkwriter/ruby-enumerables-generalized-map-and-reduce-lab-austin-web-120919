85efd06418e30e468a6271013f823bef404630ef
Bretton B Holmes <famunkwriter@me.com>
2019-10-22 01:06:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,12 +8,17 @@ def map(source_array)
 array
 end
 
-def reduce(source_array, starting_point*)
-  new_array = []
-  counter = 0
-  while counter < source_array.length do
-    total += new_array.push(yield(source_array[counter]))
+def reduce(source_array, sv=nil)
+  if sv
+    sum = sv
+    counter = 0
+  else
+    num1 = source_array[0]
+    counter = 1
+  end
+  while counter < source_array.length
+    sum = yield(num1, source_array[i])
     counter += 1
   end
-total
+  sum
 end
